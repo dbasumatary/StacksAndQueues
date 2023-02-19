da7419a082bffd71f8e723fff1615dccc86d76bd
dbasumatary <basudhondip@gmail.com>
2023-02-20 02:15:37 +0530

    Added: Program to peak and pop fron the stack till empty

--- src/com/stackandqueue/LinkedList.java
+++ src/com/stackandqueue/LinkedList.java
@@ -1,12 +1,36 @@
 package com.stackandqueue;
+import java.util.EmptyStackException;
 /*creating a stack of 56->30->70*/
 
 public class LinkedList {
     private MyNode head;
+    private int size;
     public void push(int data){                       //method creation
         MyNode tempNode = new MyNode(data);
         tempNode.next = head;                        //the previous head will be stored in tempNode
         head = tempNode;                             //the head will now point to the tempNode
+        size++;
+    }
+    public boolean isEmpty(){                       //check for empty stack
+        return size == 0;
+    }
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();         //method in stack class to show that the stack is empty
+        }
+        return head.data;                            //the top element will be displayed but not removed
+    }
+    public int pop(){                             //pop method
+        if(isEmpty()){                            //check if the stack is empty
+            throw new EmptyStackException();
+        }
+        int finalResult = head.data;              //the top element will be removed in pop method
+        head = head.next;                         //after pop, the next node will be the head
+        size--;                                   //the size will also decrease
+        return finalResult;
+    }
+    public int size(){                             //checking the size of stack
+        return size;
     }
     public void display(){                          //display method
         MyNode currentNode = head;
--- src/com/stackandqueue/StackMain.java
+++ src/com/stackandqueue/StackMain.java
@@ -8,5 +8,22 @@ public class StackMain {
         stack.push(30);
         stack.push(56);
         stack.display();
+        System.out.println();
+        System.out.println("The initial size of stack is " + stack.size());         //initial size is 3
+
+        System.out.println("\nPeek : " + stack.peek());               //peek returns the top element from stack without removing
+        System.out.println("Pop 1 : " + stack.pop());                                  //removes top item from the stack
+        System.out.println("After pop, the first element of the stack will be removed");
+        stack.display();
+        System.out.println("\nAfter first pop ,the size of stack is " + stack.size());     //after doing pop(), now the size will be 2
+        System.out.println();
+
+        System.out.println("Pop 2 : " + stack.pop());
+        stack.display();
+        System.out.println("\nAfter second pop ,the size of stack is " + stack.size());
+        System.out.println();
+
+        System.out.println("Pop 3 : " + stack.pop());
+        System.out.println("After third pop ,the size of stack is " + stack.size());
     }
 }
